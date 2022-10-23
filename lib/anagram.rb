c128ff321b2cbfd2ac97eb64cd514fd487c48b55
Ashley <ashleynjeri7@gmail.com>
2022-10-23 20:03:41 +0300

    passed tests

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize (word)
+        @word = word
+    end
+
+    def match (arr) # array of possible anagrams
+        arr.select do |letter| # .select iterates on array/hash & returns a new array/hash of all values that evaluate as true
+            letter.split('').sort == word.split('').sort
+        end
+    end
+end 
